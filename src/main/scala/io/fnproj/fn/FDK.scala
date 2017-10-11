46bc9ef6c4d6a8b649a530f11a37ed28674a91b3
Owain Lewis <owain@owainlewis.com>
2017-10-11 14:36:45 +0100

    Update FDK.scala

--- src/main/scala/io/fnproj/fn/FDK.scala
+++ src/main/scala/io/fnproj/fn/FDK.scala
@@ -3,15 +3,7 @@ package io.fnproj.fn
 import scala.io.{BufferedSource, Source}
 
 object FDK {
-  /**
-    * Consume the entire function event body as a string
-    */
   def read(): BufferedSource = Source.stdin
-
-  /**
-    * Write a string to stdout
-    *
-    * @param msg
-    */
+  
   def write(msg: String): Unit = Console.out.println(msg)
 }
