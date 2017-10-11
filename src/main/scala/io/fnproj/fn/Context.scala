61b80db1ba294dc37778de916dde79ba7bb85b65
Owain Lewis <owain@owainlewis.com>
2017-10-11 14:47:07 +0100

    Update Context.scala

--- src/main/scala/io/fnproj/fn/Context.scala
+++ src/main/scala/io/fnproj/fn/Context.scala
@@ -5,7 +5,16 @@ final class Context {
   val env: Map[String, String] = sys.env
 
   def fnContext: Map[String, String] = {
-    val fnValues = List(
+    fnValues.foldLeft(Map.empty[String, String]) { case(m, k) =>
+      env.get(k) map { v =>
+        m + (k -> v)
+      } getOrElse m
+    }
+  }
+
+  def log(msg: String): Unit = Console.err.println(msg)
+  
+  private val fnValues = List(
       "FN_APP_NAME",
       "FN_PATH",
       "FN_METHOD",
@@ -16,12 +25,4 @@ final class Context {
       "FN_HEADER",
       "FN_CALL_ID",
       "FN_REQUEST_URL")
-    fnValues.foldLeft(Map.empty[String, String]) { case(m, k) =>
-      env.get(k) map { v =>
-        m + (k -> v)
-      } getOrElse m
-    }
-  }
-
-  def log(msg: String): Unit = Console.err.println(msg)
 }
