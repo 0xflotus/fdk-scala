f6dbdff34ee1a5598c6239d7417968f6e8a9b1b1
Owain Lewis <owain@owainlewis.com>
2017-10-11 14:36:53 +0100

    Update FDK.scala

--- src/main/scala/io/fnproj/fn/FDK.scala
+++ src/main/scala/io/fnproj/fn/FDK.scala
@@ -3,6 +3,7 @@ package io.fnproj.fn
 import scala.io.{BufferedSource, Source}
 
 object FDK {
+  
   def read(): BufferedSource = Source.stdin
   
   def write(msg: String): Unit = Console.out.println(msg)
